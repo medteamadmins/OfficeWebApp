44e4b1098c8fd08793ca0e31c0b96256a5afc966
medteamadmins <143048010+medteamadmins@users.noreply.github.com>
2023-10-03 09:44:36 -0400

    Add files via upload
    Office 365 Web app

---
+++ Install.ps1
@@ -0,0 +1,38 @@
+#GPN - 4.25.2022
+#Creates shortcut on the public desktop for all users and specifies which browser to open the shortcut. Also sets custom icon file if desired. 
+#Create an icon file with convertto-icon.ps1 or https://convertio.co/jpg-ico/
+#If you do not want to specify a browser to open the shortcut, and you want it to open in the default browser, do not specify a target path to a browser. Make the target path the URL you want to use, and comment out or delete the $Shortcut.Arguments line. 
+
+#Create directory to hold icon file and copy file there
+New-Item -Path "C:\ProgramData\TMT\Office365Web" -ItemType Directory
+
+# URL of the PNG image you want to download
+$imageURL = "https://raw.githubusercontent.com/medteamadmins/MasterScript/main/Office365.ico"
+
+# Specify the local path where you want to save the downloaded PNG file
+$localFilePath = "C:\ProgramData\TMT\Office365Web\OfficeIcon.ico"
+
+# Create a WebClient object to download the image
+$webClient = New-Object System.Net.WebClient
+
+# Download the image and save it to the local path
+$webClient.DownloadFile($imageURL, $localFilePath)
+
+# Dispose of the WebClient object
+$webClient.Dispose()
+
+#---------------------------------------------
+
+
+#Shortcut creation and specify settings
+$chromeExePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
+$targetURL = "https://portal.office.com"
+$shortcutPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath("Desktop"), "Office 365 Portal.lnk")
+
+$WshShell = New-Object -ComObject WScript.Shell
+$shortcut = $WshShell.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = $chromeExePath
+$shortcut.IconLocation = "C:\ProgramData\TMT\Office365Web\OfficeIcon.ico"
+$shortcut.Arguments = $targetURL
+$shortcut.Save()
+New-Item -Path "C:\ProgramData\TMT\Office365Web\Successful.txt"
